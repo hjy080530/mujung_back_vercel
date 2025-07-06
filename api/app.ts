e258ee6ffad95f3eb4ae51984eddb32dbbea70aa
hjy080530 <bomaye119@gmail.com>
2025-07-06 23:51:34 +0900

    chore:: vercel에 맞게 파일 구조 변경

--- api/app.ts
+++ api/app.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import songRoutes from './routes/songs.route';
 import votesRoute from "./routes/votes.route";
 import searchRoute from "./routes/search.route";
-import oauthRouter from './routes/oauth';
+import oauthRouter from './routes/oauth.route';
 
 dotenv.config();
 
