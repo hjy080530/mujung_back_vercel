8ae957cbc35b8d9e1c62f6ad03f93200110f714d
hjy080530 <164326669+hjy080530@users.noreply.github.com>
2025-07-06 23:39:29 +0900

    chore ::oauth.ts

--- src/routes/oauth.ts
+++ src/routes/oauth.ts
@@ -3,20 +3,17 @@ import passport from 'passport';
 
 const router = Router();
 
-router.get('/google', (req, res, next) => {
-  const from = req.query.from || '/';
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
-    state: from
-  })(req, res, next);
-});
+router.get('/google', passport.authenticate('google', {
+  scope: ['profile', 'email'],
+  state: '/'
+}));
 
 router.get('/google/callback', passport.authenticate('google', {
   failureRedirect: '/login/fail',
   session: false,
 }), (req, res) => {
   const from = req.query.state || '/';
-  res.redirect(`https://localhost:5173/${from}`);
+  res.redirect(`https://mujung.vercel.app${from}`);
 });
 
 export default router;
