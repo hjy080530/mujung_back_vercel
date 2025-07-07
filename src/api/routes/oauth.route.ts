4a187c580d8fef290cca398b5a379e7605dcc9cc
hjy080530 <bomaye119@gmail.com>
2025-07-07 11:49:22 +0900

    chore :: 토큰 수정

--- src/api/routes/oauth.route.ts
+++ src/api/routes/oauth.route.ts
@@ -1,35 +1,25 @@
 import { Router } from 'express';
-import passport from 'passport';
-import jwt from 'jsonwebtoken';
+import passport from '../../lib/passport';
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
-function generateJwt(user: any) {
-    return jwt.sign(
-        {
-            email: user.emails[0].value,
-            name: user.displayName,
-        },
-        process.env.JWT_SECRET!,
-        { expiresIn: '1h' }
-    );
-}
-
-router.get('/google', passport.authenticate('google', {
-    scope: ['profile', 'email'],
-    state: '/'
-}));
+router.get('/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+);
 
 router.get(
     '/google/callback',
     passport.authenticate('google', { session: false }),
     (req, res) => {
         const user = req.user as any;
-        console.log('유저 정보:', user);
+        console.log('최종 유저 정보:', user);
+
+        const token = jwt.sign({ email: user.emails[0].value }, process.env.JWT_SECRET!, {
+            expiresIn: '1d',
+        });
 
         const email = user.emails[0].value;
-        const token = generateJwt(user);
-
         res.redirect(`https://mujung-three.vercel.app/?token=${token}&email=${email}`);
     }
 );
--- src/lib/passport.ts
+++ src/lib/passport.ts
@@ -1,18 +1,33 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy, Profile } from 'passport-google-oauth20';
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID!,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    callbackURL: 'https://mujung-back-vercel.vercel.app/oauth/google/callback'
-}, (
-    accessToken,
-    refreshToken,
-    profile,
-    done
-) => {
-    console.log('🟢 GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID);
-    console.log('🟢 accessToken:', accessToken);
-    console.log('🟢 profile:', profile);
-    return done(null, profile);
-}));
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            callbackURL: 'https://mujung-back-vercel.vercel.app/oauth/google/callback',
+        },
+        (
+            accessToken: string,
+            refreshToken: string,
+            profile: Profile,
+            done: (error: any, user?: any) => void
+        ) => {
+            console.log('🟢 GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID);
+            console.log('🟢 accessToken:', accessToken);
+            console.log('🟢 profile:', profile);
+
+            return done(null, profile);
+        }
+    )
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+passport.deserializeUser((user: any, done) => {
+    done(null, user);
+});
+
+export default passport;
