db79853c2656d95dcc2c59af02e645a3935bb236
hjy080530 <bomaye119@gmail.com>
2025-07-07 15:39:18 +0900

    chore :: 타입 에러 수정

--- src/api/controllers/vote.controller.ts
+++ src/api/controllers/vote.controller.ts
@@ -1,6 +1,25 @@
 import { Request, Response } from 'express';
+import supabase from "../services/supabase";
 
-export const voteForSong = async (req: Request<{},{},{ link_id: string; email: string }>, res: Response) => {
+
+export const submitVote = async (req: Request, res: Response) => {
     const { link_id, email } = req.body;
-    res.status(201).json({ message: '투표 완료!' });
+
+    if (!link_id || !email) {
+        return res.status(400).json({ message: 'link_id 또는 email 누락' });
+    }
+
+    const { data, error } = await supabase
+        .from('vote_aggregate')
+        .insert([{ link_id, email }]);
+    console.log('📦 insert result:', data);
+    console.log('❌ insert error:', error);
+
+    if (error) {
+        console.error('❌ supabase insert error:', error);
+        return res.status(500).json({ message: '서버 오류' });
+    }
+
+
+    return res.status(201).json({ message: '투표 완료', data });
 };
