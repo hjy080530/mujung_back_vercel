8d166b9bed71f8428f080921d18ba244bd37d183
hjy080530 <bomaye119@gmail.com>
2025-07-07 11:34:20 +0900

    chore :: 토큰 생성

--- src/api/routes/oauth.route.ts
+++ src/api/routes/oauth.route.ts
@@ -1,19 +1,37 @@
 import { Router } from 'express';
 import passport from 'passport';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 
+function generateJwt(user: any) {
+    return jwt.sign(
+        {
+            email: user.emails[0].value,
+            name: user.displayName,
+        },
+        process.env.JWT_SECRET!,
+        { expiresIn: '1h' }
+    );
+}
+
 router.get('/google', passport.authenticate('google', {
-  scope: ['profile', 'email'],
-  state: '/'
+    scope: ['profile', 'email'],
+    state: '/'
 }));
 
-router.get('/google/callback', passport.authenticate('google', {
-  failureRedirect: '/login/fail',
-  session: false,
-}), (req, res) => {
-  const from = req.query.state || '/';
-  res.redirect(`https://mujung-three.vercel.app${from}`);
-});
+router.get(
+    '/google/callback',
+    passport.authenticate('google', { session: false }),
+    (req, res) => {
+        const user = req.user as any;
+        console.log('유저 정보:', user);
+
+        const email = user.emails[0].value;
+        const token = generateJwt(user);
+
+        res.redirect(`https://mujung-three.vercel.app/?token=${token}&email=${email}`);
+    }
+);
 
 export default router;
