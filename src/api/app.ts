aecab267168adfd9b3e50ae52b6ef7e6423fdac2
hjy080530 <bomaye119@gmail.com>
2025-07-07 14:13:54 +0900

    chore :: 주소 수정

--- src/api/app.ts
+++ src/api/app.ts
@@ -6,19 +6,19 @@ import votesRoute from "./routes/votes.route";
 import searchRoute from "./routes/search.route";
 import oauthRouter from './routes/oauth.route';
 import passport from "passport";
-import '../lib/passport';
+import '../../lib/passport';
 
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(passport.initialize());
+
 app.use('/votes', votesRoute);
 app.use('/songs', songRoutes);
-app.use('/search',searchRoute);
-app.use(passport.initialize());
-console.log('GOOGLE_CLIENT_ID', process.env.GOOGLE_CLIENT_ID);
-console.log('GOOGLE_CLIENT_SECRET', process.env.GOOGLE_CLIENT_SECRET);
+app.use('/search', searchRoute);
 app.use('/oauth', oauthRouter);
 
 export default app;
