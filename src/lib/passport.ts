f304d0f365d5bad958661c672ad1c0967840d297
hjy080530 <bomaye119@gmail.com>
2025-07-07 11:15:53 +0900

    chore :: 변수 변경

--- src/lib/passport.ts
+++ src/lib/passport.ts
@@ -4,7 +4,7 @@ import { Strategy as GoogleStrategy, Profile } from 'passport-google-oauth20';
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID!,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    callbackURL: 'https://mujung-back.vercel.app/auth/google/callback'
+    callbackURL: 'https://mujung-back.vercel.app/oauth/google/callback'
 }, (
     accessToken: string,
     refreshToken: string,
