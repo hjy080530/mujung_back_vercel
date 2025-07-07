209811270f6df975652669ff5605f56a900da908
hjy080530 <bomaye119@gmail.com>
2025-07-07 13:41:11 +0900

    chore :: 주소 수정

--- src/api/controllers/vote.controller.ts
+++ src/api/controllers/vote.controller.ts
@@ -1,3 +1,8 @@
 import { Request, Response } from 'express';
 
-export const voteForSong = async (req: Request, res: Response): Promise<void> => {};
+export const voteForSong = async (req: Request, res: Response) => {
+    console.log('✅ 투표 시작');
+    console.log('👉 받은 데이터:', req.body);
+    console.log('✅ 투표 완료');
+    res.status(201).json({ message: '투표 완료!' });
+};
