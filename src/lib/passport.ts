fb1c726163fcd68d61dd374ecdc5a41441d9c47a
hjy080530 <bomaye119@gmail.com>
2025-07-07 11:22:26 +0900

    chore ::콘솔 추가

--- src/lib/passport.ts
+++ src/lib/passport.ts
@@ -4,12 +4,15 @@ import { Strategy as GoogleStrategy, Profile } from 'passport-google-oauth20';
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID!,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    callbackURL: 'https://https://mujung-back-vercel.vercel.app/oauth/google/callback'
+    callbackURL: 'https://mujung-back-vercel.vercel.app/oauth/google/callback'
 }, (
-    accessToken: string,
-    refreshToken: string,
-    profile: Profile,
-    done: (error: any, user?: any) => void
+    accessToken,
+    refreshToken,
+    profile,
+    done
 ) => {
+    console.log('🟢 GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID);
+    console.log('🟢 accessToken:', accessToken);
+    console.log('🟢 profile:', profile);
     return done(null, profile);
 }));
