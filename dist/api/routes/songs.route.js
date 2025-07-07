2a8fccf6694519f765b90788edb04e336171a613
hjy080530 <bomaye119@gmail.com>
2025-07-07 21:51:33 +0900

    chore: clean up server entry & dist, stabilize structure

--- dist/api/routes/songs.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const songs_controller_1 = require("../controllers/songs.controller");
-const router = (0, express_1.Router)();
-router.post('/', songs_controller_1.createSong);
-router.get('/', songs_controller_1.getSongs);
-exports.default = router;
