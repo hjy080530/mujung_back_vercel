bd7ecb6d3fbd6cca2dcce0878dd2ad4afd3a5998
hjy080530 <bomaye119@gmail.com>
2025-07-07 16:05:39 +0900

    feat :: 투표 타입 수정

--- src/api/controllers/vote.controller.ts
+++ src/api/controllers/vote.controller.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from 'express';
 import supabase from '../services/supabase';
 
-export const voteForSong = async (req: Request, res: Response): Promise<Response> => {
+export const voteForSong = async (req: Request, res: Response): Promise<void> => {
     const { link_id, user_id } = req.body;
 
     if (!link_id || !user_id) {
-        return res.status(400).json({ message: 'link_id 또는 user_id 누락' });
+        res.status(400).json({ message: 'link_id 또는 user_id 누락' });
+        return;
     }
 
     const { data: user, error: userError } = await supabase
@@ -15,8 +16,10 @@ export const voteForSong = async (req: Request, res: Response): Promise<Response
         .single();
 
     if (userError || !user) {
-        return res.status(404).json({ message: '유저 정보가 없습니다.' });
+        res.status(404).json({ message: '유저 정보가 없습니다.' });
+        return;
     }
+
     const { data: existingVote, error: duplicateError } = await supabase
         .from('votes')
         .select('*')
@@ -26,11 +29,13 @@ export const voteForSong = async (req: Request, res: Response): Promise<Response
 
     if (duplicateError) {
         console.error('중복 확인 에러:', duplicateError);
-        return res.status(500).json({ message: '서버 오류' });
+        res.status(500).json({ message: '서버 오류' });
+        return;
     }
 
     if (existingVote) {
-        return res.status(409).json({ message: '이미 투표했습니다.' });
+        res.status(409).json({ message: '이미 투표했습니다.' });
+        return;
     }
 
     const { data, error } = await supabase
@@ -38,9 +43,10 @@ export const voteForSong = async (req: Request, res: Response): Promise<Response
         .insert([{ user_id, link_id }]);
 
     if (error) {
-        console.error('supabase insert error:', error);
-        return res.status(500).json({ message: '서버 오류' });
+        console.error('❌ supabase insert error:', error);
+        res.status(500).json({ message: '서버 오류' });
+        return;
     }
 
-    return res.status(201).json({ message: '투표 완료', data });
+    res.status(201).json({ message: '투표 완료', data });
 };
